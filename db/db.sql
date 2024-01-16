acc36087b81bcba3cc688f0712d9e502a3e7fc54
Pham The Trong <trongpt@ftech.ai>
2024-01-16 18:13:21 +0700

    Update sql format

--- db/db.sql
+++ db/db.sql
@@ -1,12 +1,22 @@
 CREATE TABLE services (
-	id SERIAL PRIMARY KEY,
+	service_id SERIAL PRIMARY KEY,
     service_name VARCHAR(255),
     status VARCHAR(255),
     start_time TIMESTAMPTZ NOT NULL,
     end_time TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
     uptime VARCHAR(255),
-    metadata JSONB
+    service_metadata JSONB,
+    node_id INT references nodes(node_id)
     );
+   
+CREATE TABLE nodes (
+	node_id SERIAL PRIMARY KEY,
+    node_name VARCHAR(255),
+    node_ip VARCHAR(255),
+    node_metadata JSONB
+);
+
+
 CREATE OR REPLACE FUNCTION update_end_time_column()
 RETURNS TRIGGER AS $$
 BEGIN
@@ -21,21 +31,8 @@ FOR EACH ROW
 EXECUTE FUNCTION update_end_time_column();
 
 
+insert into services(service_name, status, start_time,end_time,uptime,node_id, service_metadata)
+values ('noservice', 'Up', '2024-01-10 15:38:03.595 +0700', '2024-01-10 15:38:03.595 +0700', '00:00:00', '1', '{"env": "stg"}');
 
-CREATE TABLE services (
-	service_id SERIAL PRIMARY KEY,
-    service_name VARCHAR(255),
-    status VARCHAR(255),
-    start_time TIMESTAMPTZ NOT NULL,
-    end_time TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-    uptime interval,
-    metadata JSONB,
-    node_id INT references nodes(node_id)
-    );
-   
-CREATE TABLE nodes (
-	node_id SERIAL PRIMARY KEY,
-    node_name VARCHAR(255),
-    node_ip VARCHAR(255),
-    metadata JSONB
-);
+insert into nodes(node_name, node_ip, node_metadata)
+values ('prod-ftech', '192.168.1.3', '{"env": "prod"}') returning node_id;
